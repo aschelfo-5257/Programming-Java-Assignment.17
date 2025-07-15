d1fbf3bb0dd1a2736919c05aa15ea216219472f8
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-15 01:07:49 -0400

    SQLClient.java

---
+++ Chapter 35:  Programming Exercise 35.1 /SQLClient.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class SQLClient extends Application {
+  
+}
